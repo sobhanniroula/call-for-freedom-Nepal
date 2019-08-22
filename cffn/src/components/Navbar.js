f3a240dfa649e534c48aff7f2c5af4aab18b1523
sobhanniroula <sobhan_niroula@yahoo.com>
2019-08-23 02:16:32 +0300

    adding bootstrap and favicons to the project

---
+++ cffn/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div className="navbar">
+                <div className="container">
+                <h4>Hello from navbar</h4>
+                </div>
+                
+            </div>
+        )
+    }
+}
