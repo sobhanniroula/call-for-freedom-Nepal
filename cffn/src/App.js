cf5295512583f2d3d84e910ef0cee7c1a1d0914c
sobhanniroula <sobhan_niroula@yahoo.com>
2019-08-24 12:40:43 +0300

    add react-router-dom

--- cffn/src/App.js
+++ cffn/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
+import TopBar from './components/TopBar';
 
 
 export default class App extends Component {
   render() {
     return (
      <React.Fragment>
+       <TopBar />
        <Navbar />
      </React.Fragment>
     )
--- cffn/src/components/Navbar.js
+++ cffn/src/components/Navbar.js
@@ -3,11 +3,24 @@ import React, { Component } from 'react'
 export default class Navbar extends Component {
     render() {
         return (
-            <div className="navbar">
-                <div className="container">
-                <h4>Hello from navbar</h4>
-                </div>
-                
+            <div className="navbar-component">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="#"><img src="/cffn-logo.png" alt="Cffn Logo"></img></a>
+                    <button class="navbar-toggler custom-navbar-toggler" type="button" data-toggle="collapse" data-target="#custom-toggler">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="custom-toggler">
+                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
         )
     }
