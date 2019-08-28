569c75c22829ff6c23584c297b123c4724e39c45
sobhanniroula <sobhan_niroula@yahoo.com>
2019-08-29 00:36:27 +0300

    modification of navbar-brand and button placement to the right

--- cffn/src/components/Navbar.js
+++ cffn/src/components/Navbar.js
@@ -4,60 +4,42 @@ export default class Navbar extends Component {
     render() {
         return (
             <div className="navbar-component">
-                {/* <nav className="navbar navbar-expand-lg navbar-light">
-                    <a className="navbar-brand" href="/"><img src="/cffn-logo.png" alt="Cffn Logo"></a>
-                    <button className="navbar-toggler custom-navbar-toggler" type="button" data-toggle="collapse" data-target="#custom-toggler">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                <nav className="navbar navbar-light navbar-expand-lg justify-content-between">
+                    <div className="container-fluid">
 
-                    <div className="collapse navbar-collapse" id="custom-toggler">
-                        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">Link</a>
-                        </li>
-                        </ul>
+                        <div className="navbar-collapse collapse dual-nav w-50 order-1 order-md-0">
+                            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/">Home</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/">Posts</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <a href="/" className="navbar-brand mx-auto d-block text-center order-0 order-sm-1 w-25">
+                            <img src="/cffn-logo.png" alt="Cffn Logo"></img>
+                        </a>
+
+                        <div className="navbar-collapse collapse dual-nav w-50 order-2">
+                            <ul className="nav navbar-nav ml-auto">
+                                <li className="nav-item"><a className="nav-link" href="">Instagram</a></li>
+                                <li className="nav-item"><a className="nav-link" href="">Facebook</a></li>
+                                <li className="nav-item"><a className="nav-link" href="">Twitter</a></li>
+                                <li className="nav-item"><a className="nav-link" href="">Email</a></li>
+                            </ul>
+                        </div>
+
+                        <button className="navbar-toggler custom-navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-nav">
+                            <span className="navbar-toggler-icon-custom"><i class="fas fa-bars"></i></span>
+                        </button>
+
                     </div>
-                </nav> */}
-
-<nav class="navbar navbar-light navbar-expand-md justify-content-between">
-  <div class="container-fluid">
-
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-nav">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="navbar-collapse collapse dual-nav w-50 order-1 order-md-0">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/">Posts</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/">Contact</a>
-        </li>
-      </ul>
-    </div>
-
-    <a href="/" class="navbar-brand mx-auto d-block text-center order-0 order-md-1 w-25">
-        <img src="/cffn-logo.png" alt="Cffn Logo"></img>
-    </a>
-
-    <div class="navbar-collapse collapse dual-nav w-50 order-2">
-      <ul class="nav navbar-nav ml-auto">
-        <li class="nav-item"><a class="nav-link" href="">Instagram</a></li>
-        <li class="nav-item"><a class="nav-link" href="">Facebook</a></li>
-        <li class="nav-item"><a class="nav-link" href="">Twitter</a></li>
-        <li class="nav-item"><a class="nav-link" href="">Email</a></li>
-      </ul>
-    </div>
-
-  </div>
-</nav>
+                </nav>
             </div>
         )
     }
