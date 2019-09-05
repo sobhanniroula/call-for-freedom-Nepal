e4ecfc3fcd4a770aa5d3d5ac67be215d2432db8b
sobhanniroula <sobhan_niroula@yahoo.com>
2019-09-05 23:32:10 +0300

    new component: messengerChat

---
+++ cffn/src/components/MessengerChat.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MessengerChat extends Component {
+    render() {
+        return (
+            <div className="messenger-chat">
+                
+            </div>
+        )
+    }
+}
